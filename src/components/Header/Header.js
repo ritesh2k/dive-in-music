96b9662ca1724fc0f0e7ec7141ff7748849b87ac
ritesh-kumar <19952553+ritesh2k@users.noreply.github.com>
2021-03-27 23:53:12 +0530

    setup dive-in-music

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./Header.scss";
+export const Header = () => {
+  return (
+    <header className="page-header">
+      <h1>Dive in Music!</h1>
+    </header>
+  );
+};
