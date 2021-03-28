49185aa4edec9823b2b8e9741a3fbc2bda785243
ritesh-kumar <19952553+ritesh2k@users.noreply.github.com>
2021-03-28 23:21:54 +0530

    add d3.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Chart } from "./components/Charts/Chart";
 import { Header } from "./components/Header/Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Chart/>
   </React.StrictMode>,
   document.getElementById("root")
 );
